8e19ebd1963d67cee7e03f822ad45f62b1d8bc78
Thiago Moabi <124590629+Tfreire455@users.noreply.github.com>
2024-08-15 16:23:47 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Configura o CORS para permitir requisições do frontend
 app.use(cors({
-  origin: 'https://casa-verde-o9tt.vercel.app' // Substitua pelo URL do seu frontend
+  origin: 'https://casa-verde-nu-eight.vercel.app' // Substitua pelo URL do seu frontend
 }));
 
 // Configura o middleware para analisar o corpo das requisições como JSON
